c069984e1295a3b067c52bacc7b2ffdb6f74a0e9
Omar685 <146872421+Omar685@users.noreply.github.com>
2024-07-19 00:40:50 +0300

    done login forms

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from flask import Flask, render_template, request, redirect, url_for, session, flash, make_response
+from flask import Flask, render_template, request, redirect, url_for, session, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_wtf.csrf import CSRFProtect
 from models import (SQLite3Executor, MariaDBExecutor)
+import pymysql
 import os
 
 app = Flask(__name__)
@@ -10,11 +11,47 @@ executor = MariaDBExecutor("localhost", "root", "123456789mM.", "alakhrah")
 executor.connect()
 
 app.secret_key = open("secret.key", 'r').read()
-app.config.update(
-  SESSION_COOKIE_HTTPONLY=True,
-  SESSION_COOKIE_SECURE=True,
-  SESSION_COOKIE_SAMESITE='Lax'
-)
+# app.config.update(
+#   SESSION_COOKIE_HTTPONLY=True,
+#   SESSION_COOKIE_SECURE=True,
+#   SESSION_COOKIE_SAMESITE='Lax'
+# )
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+  if request.method == "POST":
+    email = request.form["mailInput"]
+    password = request.form["passInput"]
+ 
+    user = executor.fetchone(f"SELECT * FROM users WHERE email='{email}' AND password='{password}';")
+    if user:
+      print(f"ID: '{user[0]}' - Name: '{user[1]}' - Email: '{user[2]}' Password: '{user[3]}'")
+      session['email'] = user[2]
+      session['password'] = user[3]
+      return redirect(url_for("index"))
+    else:
+      print("Invalid credentials", 'error')
+      return redirect(url_for("login"))
+  
+  return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+  if request.method == "POST":
+    name = request.form["name"]
+    email = request.form["email"]
+    password = request.form["passwd"]
+      
+    try:
+      executor.execute(f"INSERT INTO users(name, email, password) VALUES('{name}', '{email}', '{password}')")
+      print("Registration seccessful", 'success')
+      return redirect(url_for("login"))
+    
+    except pymysql.IntegrityError:
+      print("Email already exists", 'error')
+      return redirect(url_for("register"))
+  
+  return render_template("register.html")
 
 @app.route("/")
 def index():
@@ -22,28 +59,6 @@ def index():
     return f"Welcome {session['email']}"
   else:
     return redirect(url_for('login'))
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-  if request.method == "POST":
-    email = request.form["email"]
-    password = request.form["password"]
-
-    if not email or not password:
-      print("All fields are required", 'error')
-    
-    user = executor.fetchone(f"SELECT * FROM users WHERE email='{email}' AND password='{password}';")
-    if user:
-      print(f"ID: '{user[0]}' - Name: '{user[1]}' - Email: '{user[2]}' Password: '{user[3]}'")
-      session['email'] = user[2]
-      session['password'] = user[3]
-
-      return redirect(url_for("index"))
-    else:
-      print("Invalid credentials", 'error')
-      return redirect(url_for("login"))
-  
-  return render_template("login.html")
 
 @app.route("/profile")
 def profile():
@@ -55,4 +70,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True, host="0.0.0.0")
--- scripts/rand.py
+++ scripts/rand.py
@@ -10,6 +10,6 @@ def generate_flask_secret_key():
   secret_key = secrets.token_hex(32)
   return secret_key
 
-# with open('secret.key', 'w') as f:
-#   f.write(generate_flask_secret_key())
+with open('secret.key', 'w') as f:
+  f.write(generate_flask_secret_key())
 # print(generate_16bit_code(16))
