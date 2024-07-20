92b21d11a91fa1e00013218f62d5339c1c15dd7d
Omair Mohammed <126175145+omair-660@users.noreply.github.com>
2024-07-20 06:21:40 +0300

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -11,3 +11,51 @@
 //     nameInputRegister.val(null).removeClass("is-valid is-invalid");
 //     passInputRegister.val(null).removeClass("is-valid is-invalid");
 // }
+
+
+$(document).ready(function () {
+  let navHeight = $("nav").outerHeight(true);
+  let asideWidth = $("aside").outerWidth(true);
+  let asideHeight = $("aside").outerHeight(true);
+
+  $("aside").css("top", navHeight + "px");
+
+  function handleMediaQuery(mediaQuery) {
+    if (mediaQuery.matches) {
+      $("main").css({ "marginTop": navHeight + "px", "marginLeft": "0" });
+      $("aside").removeClass("vh-100");
+    } else {
+      $("main").css({ "marginLeft": asideWidth + "px", "marginTop": "0" });
+    }
+  }
+
+  let mediaQuery = window.matchMedia("(max-width: 780px)");
+
+  handleMediaQuery(mediaQuery);
+
+  mediaQuery.addListener(handleMediaQuery);
+
+  $("aside ul li").on("click", function () {
+    $("aside ul li").removeClass("active");
+    $("aside ul li i").removeClass("active");
+
+    $(this).addClass("active");
+    $(this).find("i").addClass("active");
+  });
+
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > asideHeight / 2) {
+      $(".top").fadeIn(500);
+    } else {
+      $(".top").fadeOut(500);
+    }
+  });
+});
+$(".top").on("click", function () {
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+});
+
+
+$(".heart").on("click", function () {
+  $(this).find("i").toggleClass("active");
+});
