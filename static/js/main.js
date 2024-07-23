0d0d06518262c2a707f4e1218665f7909a9367d5
Omair Mohammed <126175145+omair-660@users.noreply.github.com>
2024-07-23 03:05:10 +0300

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -17,31 +17,34 @@ $(document).ready(function () {
   let navHeight = $("nav").outerHeight(true);
   let asideWidth = $("aside").outerWidth(true);
   let asideHeight = $("aside").outerHeight(true);
-
   $("aside").css("top", navHeight + "px");
 
-  function handleMediaQuery(mediaQuery) {
-    if (mediaQuery.matches) {
-      $("main").css({ "marginTop": navHeight + "px", "marginLeft": "0" });
-      $("aside").removeClass("vh-100");
-    } else {
-      $("main").css({ "marginLeft": asideWidth + "px", "marginTop": "0" });
+  function mediaScreen() {
+    function handleMediaQuery(mediaQuery) {
+      if (mediaQuery.matches) {
+        $("main").css({ "marginTop": navHeight * 2 + "px", "marginLeft": "0" });
+        $("aside").removeClass("vh-100");
+        $(".show .close").css("top", navHeight * 2 + "px");
+
+      } else {
+        $(".show .close").css("top", navHeight + "px");
+        $("main").css({ "marginLeft": asideWidth + "px", "marginTop": asideWidth + "px" });
+        $("aside").addClass("vh-100");
+
+      }
     }
+
+    let mediaQuery = window.matchMedia("(max-width: 780px)");
+
+    handleMediaQuery(mediaQuery);
+
+    mediaQuery.addListener(handleMediaQuery);
   }
 
-  let mediaQuery = window.matchMedia("(max-width: 780px)");
+  mediaScreen()
 
-  handleMediaQuery(mediaQuery);
 
-  mediaQuery.addListener(handleMediaQuery);
 
-  $("aside ul li").on("click", function () {
-    $("aside ul li").removeClass("active");
-    $("aside ul li i").removeClass("active");
-
-    $(this).addClass("active");
-    $(this).find("i").addClass("active");
-  });
 
   $(window).on("scroll", function () {
     if ($(window).scrollTop() > asideHeight / 2) {
@@ -58,4 +61,54 @@ $(".top").on("click", function () {
 
 $(".heart").on("click", function () {
   $(this).find("i").toggleClass("active");
+});
+
+
+$(".cover-image img").on("click", function () {
+  let imgSrc = $(this).attr("src");
+  $(".show img").attr("src", imgSrc)
+  $(".show").fadeIn(500).addClass("d-flex")
+});
+
+
+$(".show .close").on("click", function () {
+  $(".show").fadeOut(500, function () {
+    $(".show").removeClass("d-flex")
+  })
+})
+
+
+$(".show-more p").hover(
+  function () {
+    $(this).addClass("text-primary-emphasis");
+  },
+  function() {
+    $(this).removeClass("text-primary-emphasis");
+  }
+);
+
+$(".show-more p").on("click", function(){
+  const $this = $(this);
+  $this.siblings(".more-info").toggle(300);
+  $this.text($this.text() === 'Show more' ? 'Show less' : 'Show more');
+  $this.parent().toggleClass("open");
+});
+
+// cooments show
+$(".post .comment").on("click", function() {
+  $(this).siblings(".body-comment").fadeIn(300).addClass("d-flex");
+});
+
+$(".post .close").on("click", function() {
+  $(this).closest(".body-comment").fadeOut(300).removeClass("d-flex");
+});
+
+// share show
+
+$(".post .share").on("click", function() {
+  $(this).siblings(".body-share").fadeIn(300).addClass("d-flex");
+});
+
+$(".post .close").on("click", function() {
+  $(this).closest(".body-share").fadeOut(300).removeClass("d-flex");
 });
